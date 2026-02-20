7e390198179278001443fb92323a690b57cc704f
Srujan Naik <srujannaik214@gmail.com>
2026-02-21 00:17:17 +0530

    Update Calculater.java

--- src/Calculater.java
+++ src/Calculater.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Calculater {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Simple Java Calculator");
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose operation:");
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+
+        System.out.print("Enter choice (1-4): ");
+        int choice = sc.nextInt();
+
+        double result = 0;
+
+        switch(choice) {
+            case 1:
+                result = num1 + num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case 2:
+                result = num1 - num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case 3:
+                result = num1 * num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case 4:
+                if(num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result = " + result);
+                } else {
+                    System.out.println("Error! Cannot divide by zero.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid choice.");
+        }
+
+        sc.close();
+    }
 }
